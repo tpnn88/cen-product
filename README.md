90abbee7c4d2fcaf4af2a979c948e3e665ee9b7e
phuongtt189 <tangthanhphuongbk3i@gmail.com>
2021-03-22 08:41:45 +0700

    modify

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+"build": "vue-cli-service build",
---
+++ src/vCenProduct.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Plugin sample</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "vCenProduct"
+    }
+</script>
+
+<style scoped>
+
+</style>
